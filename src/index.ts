64552c23dd72c19cf5ca6cc684562036125b1a45
Matthew Sells <matthew.m.sells@gmail.com>
2021-11-29 06:24:42 -0600

    add linter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('it works!')
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️Server⚡️: App listening on port ${PORT}`);
+});
